92268c958bdc9421a6ed41f7b162f7d5ee011558
Patryk Ksiazek <ksiazekpatryk@gmail.com>
2026-05-27 18:05:18 +0200

    Configuration/ Changes

--- agent1.ts
+++ agent1.ts
@@ -1,10 +1,11 @@
 import {createAgent} from "langchain";
+import "dotenv/config"
 
 const agent = createAgent(
     {model: "claude-sonnet-4-6"}
     );
 
-const response = agent.invoke({
+const response = await agent.invoke({
     messages: 
     [{role: "user", content: "What is sum of 2+2"}]
 });
