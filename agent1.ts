7a86f5151fbb961b975c5d32a85c4a42d45c0a0d
Patryk <ksiazekpatryk@gmail.com>
2026-06-09 16:35:33 +0200

    Adding_Tool_Langchain

--- agent1.ts
+++ agent1.ts
@@ -1,14 +1,36 @@
-import {createAgent} from "langchain";
+import {createAgent, tool} from "langchain";
 import "dotenv/config"
+import z from "zod";
+
+//20-30{city: "New York"}
+
+const getWeather = tool( ()=>{
+    //${input.city} - by getting weather - returned Sunny
+    return 'Its sunny in ${input.city}'
+    }, 
+    {
+    name: "getWeather",
+    description: "Get the weather for a given city",
+    schema: z.object({
+        city: z.string()
+        })
+    }
+);
+
+
 
 const agent = createAgent(
-    {model: "claude-haiku-4-5-20251001"}
+    {model: "claude-haiku-4-5-20251001",
+     tools: [getWeather],
+    },
     );
 
 const response = await agent.invoke({
     messages: 
-    [{role: "user", content: "What is sum of 2+2"}]
+    [{role: "user", content: "What is weather in New York?"}]
 });
-console.log(response);
+//console.log(response);
+const longMessages = response.messages[response.messages.length-1].content
+console.log(longMessages);
 
 
