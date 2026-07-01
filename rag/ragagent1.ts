1429f504a245f62fe1cfd87b98560a3eafc565b8
Patryk <ksiazekpatryk@gmail.com>
2026-07-01 16:54:52 +0200

    Add textsplitters dependency and update ragagent1 to utilize RecursiveCharacterTextSplitter

--- rag/ragagent1.ts
+++ rag/ragagent1.ts
@@ -1,7 +1,12 @@
 
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
 
 const loader = new PDFLoader("/users/patrykksiazek/downloads/ProjectDocs/nke-10k-2023.pdf")
 const docs = await loader.load()
 console.log(docs)
 console.log(docs[0].pageContent)
+
+const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
+const chunks = await splitter.splitDocuments(docs)
+console.log(chunks)
