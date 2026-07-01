a1ee4a1f98dc323b05ff631274741eb5108a191d
Patryk <ksiazekpatryk@gmail.com>
2026-07-01 17:29:20 +0200

    Refactor ragagent1 to improve document processing and add similarity search functionality

--- rag/ragagent1.ts
+++ rag/ragagent1.ts
@@ -1,12 +1,24 @@
 
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf"
-import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters"
+import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "@langchain/classic/vectorstores/memory";
+import "dotenv/config";
 
 const loader = new PDFLoader("/users/patrykksiazek/downloads/ProjectDocs/nke-10k-2023.pdf")
 const docs = await loader.load()
 console.log(docs)
 console.log(docs[0].pageContent)
 
-const splitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
-const chunks = await splitter.splitDocuments(docs)
-console.log(chunks)
+const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000, chunkOverlap: 200 })
+const allSplits = await textSplitter.splitDocuments(docs)
+console.log(allSplits)
+
+const embeddings = new OpenAIEmbeddings({ model: "text-embedding-3-large" })
+
+const vectorStore = await MemoryVectorStore.fromDocuments(allSplits, embeddings)
+
+const results = await vectorStore.similaritySearch("When was Nike incorporated?");
+
+console.log(results)
+
