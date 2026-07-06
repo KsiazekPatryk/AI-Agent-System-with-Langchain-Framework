169aeadfae078a7f863060db7125ced929baae16
Patryk <ksiazekpatryk@gmail.com>
2026-07-06 09:53:45 +0200

    feat: initialize MultiServerMCPClient and create agent with tools

--- rag/ragagent6.ts
+++ rag/ragagent6.ts
@@ -1,3 +1,4 @@
+import { createAgent } from "langchain";
 import { MultiServerMCPClient } from "@langchain/mcp-adapters";
 
 const client = new MultiServerMCPClient({
@@ -9,4 +10,14 @@ const client = new MultiServerMCPClient({
         args : ["/Users/patrykksiazek/Downloads/mcp-ecommerce-crud/dist/mcp/server.js"],
     }
 })
+
+
+const mcpTools = await client.getTools();
+
+
+createAgent({
+    model: "claude-sonnet-4-5-20250929",
+    tools: [...mcpTools]
+})
+
 
